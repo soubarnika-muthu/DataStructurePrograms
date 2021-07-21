4ffdc62759c6b36e1997e68af1ef6ae2d6fc0bf1
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-07-21 15:02:02 +0530

    [Soubarnika]P6-Using hashing function to search number in a slot

---
+++ HashTable/Hash.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    public class Hash<T>
+    {
+        //creating linked list for each slot to avoid collision
+        private readonly int listSize;
+        private LinkedList<T>[] item;
+        public Hash(int size)
+        {
+            this.listSize = size;
+            this.item = new LinkedList<T>[size];
+        }
+        //get the slot number
+        public int GetArrayPosition(T key)
+        {
+            int position = key.GetHashCode() % listSize;
+            return (Math.Abs(position));
+
+        }
+        //if linked is created for particular slot then get the linked list
+        //else create new linked list
+        protected LinkedList<T> GetLinkedList(int position)
+        {
+            LinkedList<T> linkedList = item[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<T>();
+                item[position] = linkedList;
+            }
+            return linkedList;
+        }
+        //add the node to the slot
+        public void AddNode(T value)
+        {
+            int position = GetArrayPosition(value);
+            LinkedList<T> linkedList = GetLinkedList(position);
+            linkedList.AddLast(value);
+        }
+        //method to display element in each slot
+        public string display()
+        {
+            string result = "";
+            for (int i = 0; i < listSize; i++)
+            {
+                LinkedList<T> linkedList = item[i];
+                if (linkedList == null)
+                {
+                    continue;
+                }
+                foreach (T list in linkedList)
+                {
+                    Console.WriteLine("value:{0}", list);
+                    result = list.ToString() + " " + result;
+                }
+            }
+            return result;
+        }
+        public int Search(T value)
+        {
+            int position = GetArrayPosition(value);
+            LinkedList<T> linkedList = GetLinkedList(position);
+            if (linkedList == null)
+            {
+                return 0;
+            }
+            else
+            {
+                int found = 0;
+                foreach (T i in linkedList)
+                {
+                    if (i.Equals(value))
+                    {
+                        found = 1;
+                    }
+                }
+                return found;
+            }
+        }
+    }
+
+}
---
+++ HashTable/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace HashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creat the object for hash class
+            Hash<int> hash = new Hash<int>(10);
+            Console.WriteLine("search a number using the hash table");
+            string filePath = @"C:\Users\hp\source\repos\DataStructurePrograms\HashTable\NumberFile.txt";
+            string text = File.ReadAllText(filePath);
+            Program.ReadInput(text, hash);
+            Console.WriteLine("Enter the number to search:");
+            int search = Convert.ToInt32(Console.ReadLine());
+            //if the element is present write the into the list 
+            if (hash.Search(search) == 1)
+            {
+                Console.WriteLine("{0} is found", search);
+                File.WriteAllText(filePath, hash.display());
+            }
+            else
+            {
+                hash.AddNode(search);
+                Console.WriteLine("{0} is not found", search);
+                File.WriteAllText(filePath, hash.display());
+            }
+        }
+        //read input from file and store it in hash table
+        public static void ReadInput(string text, Hash<int> hash)
+        {
+            int number;
+            string[] textArray = text.Split(" ");
+            for (int i = 0; i < textArray.Length - 1; i++)
+            {
+                if (!(textArray[i].Equals(" ")))
+                {
+                    number = Convert.ToInt32(textArray[i]);
+                    hash.AddNode(number);
+                }
+            }
+        }
+    }
+}
